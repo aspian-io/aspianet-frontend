ea9755b097e2410cc5752b4f780eaf681dfb609c
Omid Rouhani <o.rohani@gmail.com>
2022-08-22 20:25:11 +0430

    Initial Commit

---
+++ src/store/slices/slices.ts
@@ -0,0 +1,3 @@
+export enum SlicesEnum {
+  USER = 'USER'
+}
---
+++ src/store/slices/userSlice.ts
@@ -0,0 +1,40 @@
+import { createSlice, Draft, PayloadAction } from "@reduxjs/toolkit";
+import { SlicesEnum } from "./slices";
+
+export interface IUserState {
+  username: string;
+  password: string;
+}
+
+const initialState: IUserState = {
+  username: '',
+  password: ''
+} as const;
+
+export const userSlice = createSlice( {
+  name: SlicesEnum.USER,
+  initialState,
+  reducers: {
+    setUserName: (
+      state: Draft<IUserState>,
+      action: PayloadAction<typeof initialState.username>
+    ) => {
+      state.username = action.payload;
+    },
+    setPassword (
+      state: Draft<IUserState>,
+      action: PayloadAction<typeof initialState.password>
+    ) {
+      state.password = action.payload;
+    }
+  }
+} );
+
+// A small helper of user state for `useSelector` function.
+export const getUserState = ( state: { user: IUserState; } ) => state.user;
+
+// Exports all actions
+export const userActions = userSlice.actions;
+
+// Export reducer
+export const userReducer = userSlice.reducer;
